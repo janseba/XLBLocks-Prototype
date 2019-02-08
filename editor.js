45d05c9464205b03836817f1d23a02451894ce17
Bas Jansen <bas.jansen@infoaction.nl>
2019-02-08 07:25:31 +0100

    Knop stuurt string naar taskpane

--- editor.js
+++ editor.js
@@ -4,14 +4,12 @@
 		.then(function() {
 			$(document).ready(function () {
 				// TODO1: Assign handler to the OK button.
-				$('#ok-button').click(sendStringToParentPage);
-				console.log('Dit is een test');
+				$('#save-formulas').click(sendStringToParentPage);
 				delete window.prompt;
 			});
 		});
 	// TODO2: Create the OK button handler
 	function sendStringToParentPage() {
-		console.log('Er is geklikt');
 		var testString = localStorage.getItem("formulaID");
 		console.log(testString);
 		Office.context.ui.messageParent(testString);
