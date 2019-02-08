ef78dacd8ee453f48cd1653293ef2d3a7fd633ae
Bas Jansen <bas.jansen@infoaction.nl>
2019-02-08 15:38:51 +0100

    Nieuwe versie van Blockly

--- blockly/blocks/math.js
+++ blockly/blocks/math.js
@@ -38,7 +38,7 @@ goog.require('Blockly');
 
 /**
  * Unused constant for the common HSV hue for all blocks in this category.
- * @deprecated Use Blockly.Msg.MATH_HUE. (2018 April 5)
+ * @deprecated Use Blockly.Msg['MATH_HUE']. (2018 April 5)
  */
 Blockly.Constants.Math.HUE = 230;
 
@@ -372,6 +372,29 @@ Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
     "colour": "%{BKY_MATH_HUE}",
     "tooltip": "%{BKY_MATH_RANDOM_FLOAT_TOOLTIP}",
     "helpUrl": "%{BKY_MATH_RANDOM_FLOAT_HELPURL}"
+  },
+
+  // Block for calculating atan2 of [X] and [Y].
+  {
+    "type": "math_atan2",
+    "message0": "%{BKY_MATH_ATAN2_TITLE}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "X",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "Y",
+        "check": "Number"
+      }
+    ],
+    "inputsInline": true,
+    "output": "Number",
+    "colour": "%{BKY_MATH_HUE}",
+    "tooltip": "%{BKY_MATH_ATAN2_TOOLTIP}",
+    "helpUrl": "%{BKY_MATH_ATAN2_HELPURL}"
   }
 ]);  // END JSON EXTRACT (Do not delete this comment.)
 
--- editor.js
+++ editor.js
@@ -5,18 +5,24 @@
 			$(document).ready(function () {
 				// TODO1: Assign handler to the OK button.
 				$('#save-formulas').click(sendStringToParentPage);
+				$('#refresh-page').click(refreshPage);
 				delete window.prompt;
-				var blocklyWorkspace = localStorage.getItem("BlocklyWorkspace");
-				var xml = Blockly.Xml.textToDom(blocklyWorkspace);
-				Blockly.Xml.domToWorkspace(xml, workspace);
+				var xml_text = localStorage.getItem("BlocklyWorkspace");
+				console.log("receive: " +xml_text);
+				//var xml = Blockly.Xml.textToDom(xml_text);
+				Blockly.Xml.domToBlock(xml_text, workspace);
 			});
 		});
 	// TODO2: Create the OK button handler
 	function sendStringToParentPage() {
-		var testBlock = workspace.getTopBlocks(true);
-		var xml = Blockly.Xml.blockToDom(block);
-		var xml_text = Blockly.Xml.domToPrettyText(xml);
-		console.log(xml_text);
+		var testBlock = workspace.getBlocksByType("definenamedranges", false);
+		var xml = Blockly.Xml.blockToDom(testBlock[0]);
+		var xml_text = Blockly.Xml.domToText(xml);
+		console.log("send: " + xml_text);
 		Office.context.ui.messageParent(xml_text);
+	}
+
+	function refreshPage() {
+		location.reload();
 	}
 }());
