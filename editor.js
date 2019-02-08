ca78e11c24b5008809b13d7a0f87461c06a09dd5
Bas Jansen <bas.jansen@infoaction.nl>
2019-02-08 16:44:50 +0100

    Saven van 1 block werkt nu

--- editor.js
+++ editor.js
@@ -7,10 +7,11 @@
 				$('#save-formulas').click(sendStringToParentPage);
 				$('#refresh-page').click(refreshPage);
 				delete window.prompt;
-				var xml_text = localStorage.getItem("BlocklyWorkspace");
+				//var xml_text = localStorage.getItem("BlocklyWorkspace");
+				var xml_text = "<xml xmlns=\"http://www.w3.org/1999/xhtml\"><block id=\"en:~K%%+@iqU2{oG6?Z/\" type=\"definenamedranges\" /></xml>"
 				console.log("receive: " +xml_text);
-				//var xml = Blockly.Xml.textToDom(xml_text);
-				Blockly.Xml.domToBlock(xml_text, workspace);
+				var xml = Blockly.Xml.textToDom(xml_text);
+				Blockly.Xml.domToWorkspace(xml, workspace);
 			});
 		});
 	// TODO2: Create the OK button handler
@@ -18,6 +19,8 @@
 		var testBlock = workspace.getBlocksByType("definenamedranges", false);
 		var xml = Blockly.Xml.blockToDom(testBlock[0]);
 		var xml_text = Blockly.Xml.domToText(xml);
+		xml_text = "<xml xmlns=\"http://www.w3.org/1999/xhtml\">" + xml_text;
+		xml_text = xml_text + "</xml>";
 		console.log("send: " + xml_text);
 		Office.context.ui.messageParent(xml_text);
 	}
